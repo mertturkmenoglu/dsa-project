92d0f2a14f101836551458203f14cd4fc36287b6
Mert <mertturkmenoglu99@gmail.com>
2019-05-14 08:10:55 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/dsa_project.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dsa_project.dir/main.c.o"
+  "dsa_project.pdb"
+  "dsa_project"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/dsa_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
