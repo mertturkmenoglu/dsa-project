1d90145abe5a5a83dc5867c1c8671ee4e8c37fb6
Mert <mertturkmenoglu99@gmail.com>
2019-05-20 00:20:33 +0300

    Add choiceHandler and refactor names

--- main.c
+++ main.c
@@ -5,47 +5,19 @@
  * if you can convert the first word to second word and if you can,
  * print the path.
  *
+ * Written and tested on Linux Mint 19 Cinnamon
+ * Kernel 4.15.0-47-generic
+ * gcc (Ubuntu 7.3.0-27ubuntu1~18.04) 7.3.0
+ * GNU Make 4.1
+ * Built for x86_64-pc-linux-gnu
  *
+ * IDE:
+ * CLion 2019.1.2
+ * Build #CL-191.6707.69, built on April 18, 2019
  *
- * You must use graph data structure.
- * Nodes of the graph must be words.
- * If you can convert a word to another one with changing one letter on each
- * step, there is a connection between these two words.
- * There is a connection between "prove" and "prose".
- * But there is no connection between "prove" and "wrest"
- *
- * All words are has a length of five.
- * All letters are lowercase.
- * You must use
- * <a href="https://drive.google.com/open?id=1AoOQ8swqJ_bzUBRz1c1xDI9cZCyzUP9W">
- * kelime.txt</a>
- *
- * Create the word graph(12 pointes): Each word on the file must be a node.
- * Create an adjaceny matrix of NxN where 1-st word on the
- * file is the first node etc. For this node, check all other nodes.
- * If you can connect them(see above for explanation) mark the relevant
- * row-column element as 1, otherwise 0.
- *
- * Write a function shows that you create the graph correctly.
- * For given two input, it writes if there is a connection or not.
- *
- * Find if there is a transformation between two words(48 points):
- * If there is, show how many steps it takes.
- *
- * You must use BFS.
- * Steps of BFS:
- *      * Add the first word(node) to queue.
- *      * Do these steps until queue becomes empty or you
- *          reach the final node(second word):
- *          * Remove the first element of the queue.
- *          * If this the second word, it is done.
- *          * If not, print the word. Add all neighbour nodes that
- *              haven't added to queue. Use another array for this.
- *          * If queue is empty, there is no transformation.
- * First word: beer     Second word: beat
- * Way-1: beer -> bear -> fear -> feat -> beat           4 letter changing
- * Way-2: beer -> bear -> dear -> fear -> feat -> beat   5 letter changing
+ * @author Mert Turkmenoglu
  */
+
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -55,27 +27,40 @@
 
 #define MAX_WORD_LENGTH 10
 #define MAX_STDIN_LENGTH 255
-#define KELIME_FILE_LINE_COUNT 2415
+
 
 struct Node {
     char word[MAX_WORD_LENGTH];
 };
+
 
 struct Queue {
     struct QueueNode *front;
     struct QueueNode *rear;
 };
 
+
 struct QueueNode{
     struct Node value;
     struct QueueNode *next;
 };
 
+
+int bfs(int **matrix, struct Node *wordList, int wordCount, int startingPoint, int endingPoint);
+
+void bfsHandler(int **matrix, struct Node *wordList, int wordCount);
+
 int createAdjacencyMatrix(FILE *fptr, int **matrix, struct Node *wordList, int lineCount);
 
-int connection(char *first, char *second);
+int choiceHandler(int choice, int **matrix, struct Node *wordList, int lineCount);
+
+int connection(const char *first, const char *second);
+
+void connectionHandler();
 
 int fileLineCount(FILE *fptr);
+
+int getIndex(struct Node *wordList, const char str[MAX_WORD_LENGTH], int wordCount);
 
 void printMatrix(int **matrix, struct Node *wordList, int n);
 
@@ -83,27 +68,23 @@ void printMenu();
 
 void printMatrixHandler(int **matrix, struct Node *wordList, int lineCount);
 
-void connectionHandler();
-
-void bfsHandler(int **matrix, struct Node *wordList, int wordCount);
-
-int bfs(int **matrix, struct Node *wordList, int wordCount, int startingPoint, int endingPoint);
-
-int getIndex(struct Node *wordList,  char str[MAX_WORD_LENGTH], int wordCount);
-
 struct QueueNode* newNode(struct Node value);
 
 struct Queue *createQueue();
 
-void enQueue(struct Queue *q, struct Node value);
+void enqueue(struct Queue *q, struct Node value);
 
-struct QueueNode *deQueue(struct Queue *q);
+struct QueueNode *dequeue(struct Queue *q);
 
 void printQueue(struct Queue *q);
 
 void printNeighbours(int **matrix, struct Node *wordList, char str[MAX_WORD_LENGTH], int wordCount);
 
+void printNeighboursHandler(int **matrix, struct Node *wordList, int lineCount);
+
+
 extern int errno;
+
 
 int main() {
     FILE *fptr = NULL;
@@ -115,7 +96,7 @@ int main() {
     int choice;
     char *charptr;
     char str[MAX_STDIN_LENGTH];
-    int flag = 1;
+    int flag;
     int errnum;
 
     fptr = fopen(stdPath, "r");
@@ -171,22 +152,7 @@ int main() {
         scanf("%s", str);
         choice = strtol(str, &charptr, 10);
 
-        switch (choice) {
-            case 1:
-                printMatrixHandler(matrix, wordList, lineCount);
-                break;
-            case 2:
-                connectionHandler();
-                break;
-            case 3:
-                bfsHandler(matrix, wordList, lineCount);
-                break;
-            case 0:
-                flag = 0;
-                break;
-            default:
-                printf("Invalid input\n");
-        }
+        flag = choiceHandler(choice, matrix, wordList, lineCount);
 
     } while(flag == 1);
 
@@ -195,6 +161,40 @@ int main() {
     free(wordList);
 
     return 0;
+}
+
+
+void printNeighboursHandler(int **matrix, struct Node *wordList, int lineCount) {
+    char str[MAX_WORD_LENGTH];
+
+    printf("\nEnter your word: ");
+    scanf("%s", str);
+
+    printNeighbours(matrix, wordList, str, lineCount);
+}
+
+
+int choiceHandler(int choice, int **matrix, struct Node *wordList, int lineCount) {
+    switch (choice) {
+        case 1:
+            printMatrixHandler(matrix, wordList, lineCount);
+            break;
+        case 2:
+            connectionHandler();
+            break;
+        case 3:
+            bfsHandler(matrix, wordList, lineCount);
+            break;
+        case 4:
+            printNeighboursHandler(matrix, wordList, lineCount);
+            break;
+        case 0:
+            break;
+        default:
+            printf("Invalid input\n");
+    }
+
+    return (choice == 0) ? 0 : 1;
 }
 
 
@@ -237,7 +237,7 @@ int createAdjacencyMatrix(FILE *fptr, int **matrix, struct Node *wordList, int l
 }
 
 
-int connection(char *first, char *second) {
+int connection(const char *first, const char *second) {
     size_t len = 5;
     size_t i = 0;
     int counter = 0;
@@ -305,6 +305,7 @@ void printMenu() {
     printf("1- Print Adjacency Matrix\n");
     printf("2- isDifferentOneLetter\n");
     printf("3- isTransformable\n");
+    printf("4- printNeighbours");
     printf("0- Exit\n");
     printf("-------------------------\n");
 }
@@ -369,7 +370,7 @@ void bfsHandler(int **matrix, struct Node *wordList, int wordCount) {
 int bfs(int **matrix, struct Node *wordList, int wordCount, int startingPoint, int endingPoint) {
     struct Queue *q = createQueue();
 
-    enQueue(q, wordList[startingPoint]);
+    enqueue(q, wordList[startingPoint]);
     assert(q->front != NULL);
     printQueue(q);
 
@@ -379,21 +380,21 @@ int bfs(int **matrix, struct Node *wordList, int wordCount, int startingPoint, i
     int i;
 
     while(q->front != NULL) {
-        struct Node v = deQueue(q)->value;
+        struct Node v = dequeue(q)->value;
         printf("\nCurrent node: %s\n", v.word);
         int index = getIndex(wordList, v.word, wordCount);
 
         for (i = 0; i < wordCount; i++) {
-            // Komşu mu?
+            // Is it a neighbour?
             if(matrix[index][i] == 1) {
                 int j = 0;
                 while ((j < 5) && (wordList[endingPoint].word[j] == wordList[i].word[j]))
                     j++;
                 if (j == 5)
                     return 1;
-                // ziyaret edilmiş mi?
+                // Is it visited?
                 if (visited[i] != 1) {
-                    enQueue(q, wordList[i]);
+                    enqueue(q, wordList[i]);
                     visited[i] = 1;
                 }
             }
@@ -405,7 +406,7 @@ int bfs(int **matrix, struct Node *wordList, int wordCount, int startingPoint, i
 }
 
 
-int getIndex(struct Node *wordList,  char str[MAX_WORD_LENGTH], int wordCount) {
+int getIndex(struct Node *wordList,  const char str[MAX_WORD_LENGTH], int wordCount) {
     int i = 0;
     char tmp[MAX_WORD_LENGTH];
     while (i < wordCount) {
@@ -442,7 +443,7 @@ struct Queue *createQueue() {
 }
 
 
-void enQueue(struct Queue *q, struct Node value) {
+void enqueue(struct Queue *q, struct Node value) {
     struct QueueNode *temp = newNode(value);
 
     if (q->rear == NULL) {
@@ -456,7 +457,7 @@ void enQueue(struct Queue *q, struct Node value) {
 }
 
 
-struct QueueNode *deQueue(struct Queue *q) {
+struct QueueNode *dequeue(struct Queue *q) {
     if (q->front == NULL)
         return NULL;
 
