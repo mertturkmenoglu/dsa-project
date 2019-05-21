bcb84a279805805361e1aac6c22fb698a20fcc04
Mert <mertturkmenoglu99@gmail.com>
2019-05-21 23:53:33 +0300

    Remove Google Test files

--- cmake-build-debug/lib/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mert/codes/dsa-project/lib/googletest/googletest
-# Build directory: /home/mert/codes/dsa-project/cmake-build-debug/lib/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
